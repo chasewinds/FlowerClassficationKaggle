a5c0c898c08001be76ac7e1f1014eb72f387ecee
Developer <rubans@Rubans.local>
2018-06-09 18:13:40 +0800

    laptop

--- easy_conv.py
+++ easy_conv.py
@@ -181,8 +181,7 @@ def main(unused_argv):
         shuffle=True)
     mnist_classifier.train(
         input_fn=train_input_fn,
-        steps=200000,
-        hooks=[logging_hook])
+        steps=200000)
 
     # Evaluate the model and print results
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(
